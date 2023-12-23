21140b0f2d928eae9ded5fa243e44fff8a13e884
IvanVnucec <vnucec.ivan@gmail.com>
2023-12-23 23:52:57 +0100

    scrape comment reactions

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ class Comment:
     author: str
     content: str
     date: datetime.datetime
-    # TODO: add replies
+    reactions: list['Comment'] = None
+
+    def __str__(self) -> str:
+        # TODO: format date to hh:mm dd.mm.yyyy
+        return f"{self.author} ({self.date}): '{self.content[:10]}'"
 
 class Vecernji:
     def __init__(self):
@@ -33,13 +37,27 @@ class Vecernji:
             if last_page:
                 break
             page += 1
-        articles_length_scraped = int(soup.find("div", class_="article-stats__number").text)
-        assert len(articles) == articles_length_scraped
+        articles_to_parse = int(soup.find("div", class_="article-stats__number").text)
+        assert len(articles) == articles_to_parse
         return articles
+
+    def get_comment_reactions(self, comment_id:int) -> list[Comment]:
+        reactions = []
+        url = f"{self.base_url}/forum/snippet-post/{comment_id}"
+        response = self._http_get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        comment_boxes = soup.find("div", class_="comment-replies").find_all("div", class_="comment-box")
+        for box in comment_boxes:
+            # TODO: see if this code is in a get_comments function also
+            author = str(box.find("div", class_="comment-card__author-name").text).strip()
+            date = datetime.datetime.strptime(str(box.find("div", class_="comment-card__report-time").text).strip(), "%H:%M %d.%m.%Y.")
+            content = str(box.find("div", class_="comment-card__text").p.text).strip()
+            reactions.append(Comment(author, content, date))
+        return reactions
 
     def get_comments(self, article_url:str) -> list[Comment]:
         page = 1
-        comments = []
+        comments:list[Comment] = []
         while True:
             url = f"{article_url}/komentari?order=-created_date&page={page}"
             try:
@@ -51,13 +69,17 @@ class Vecernji:
                     raise
             soup = BeautifulSoup(response.text, "html.parser")
             items = soup.find("div", class_="component__content").find_all("div", class_="card-group__item")
-            # TODO: also scrape replies (and then add assert with scraped length)
-            new_comments = map(lambda item: Comment(
-                author=str(item.find("div", class_="comment-card__author-name").text).strip(),
-                date=datetime.datetime.strptime(str(item.find("div", class_="comment-card__time").text).strip(), "%H:%M %d.%m.%Y."),
-                content=str(item.find("div", class_="comment-card__text").p.text).strip(),
-            ), items)
-            comments += list(new_comments)
+            for item in items:
+                author = str(item.find("div", class_="comment-card__author-name").text).strip()
+                date = datetime.datetime.strptime(str(item.find("div", class_="comment-card__time").text).strip(), "%H:%M %d.%m.%Y.")
+                content = str(item.find("div", class_="comment-card__text").p.text).strip()
+                reactions_exists = item.find("div", class_="comment-replies")
+                if reactions_exists:
+                    comment_id = int(item.find("div", class_="comment-card")["id"].replace("position_", ""))
+                    reactions = self.get_comment_reactions(comment_id)
+                else:
+                    reactions = []
+                comments.append(Comment(author, content, date, reactions))
             pagination = soup.find("div", class_="author__pagination")
             last_page = pagination == None or pagination.find_all("a")[-1]["href"] == "#"
             if last_page:
